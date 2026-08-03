7029001b04d19b03029513451df8921ce45dadaa
Aldo Villagra <afdanic@gmail.com>
2026-08-02 20:34:03 -0600

    El carrito fue un exito, ahora el siguiente paso es hacer el menu mas personalizado.

--- JS_archivos/carrito.js
+++ JS_archivos/carrito.js
@@ -4,6 +4,18 @@ const abrirCarrito = document.getElementById("abrirCarrito");
 
 const cerrarCarrito = document.getElementById("cerrarCarrito");
 
+/*=========================================
+ESTADO DEL CARRITO
+=========================================*/
+
+let carrito = [];
+
+const cartBody = document.getElementById("cartBody");
+
+const cartCount = document.getElementById("cartCount");
+
+const cartBtn = document.getElementById("abrirCarrito");
+
 if (abrirCarrito) {
 
     abrirCarrito.addEventListener("click", () => {
@@ -41,5 +53,89 @@ if (cartOverlay) {
         }
 
     });
+
+}
+
+function actualizarCarrito(){
+
+cartCount.textContent=
+
+carrito.reduce(
+
+(total,item)=>total+item.cantidad,
+
+0
+
+);
+
+actualizarBotonesProductos();
+
+}
+
+function actualizarBotonesProductos(){
+
+    const botones = document.querySelectorAll(".btn-ordenar");
+
+    botones.forEach(boton=>{
+
+        const nombre = boton.dataset.producto;
+
+        const producto = carrito.find(
+
+            item => item.nombre === nombre
+
+        );
+
+        if(producto){
+
+            boton.innerHTML = `✓ Agregado (${producto.cantidad})`;
+
+            boton.classList.add("producto-agregado");
+
+        }else{
+
+            boton.innerHTML = "Ordenar";
+
+            boton.classList.remove("producto-agregado");
+
+        }
+
+    });
+
+}
+
+/*=========================================
+AGREGAR PRODUCTOS
+=========================================*/
+
+function agregarAlCarrito(nombre,precio,imagen){
+
+const existente = carrito.find(
+
+producto => producto.nombre===nombre
+
+);
+
+if(existente){
+
+existente.cantidad++;
+
+}else{
+
+carrito.push({
+
+nombre,
+
+precio,
+
+imagen,
+
+cantidad:1
+
+});
+
+}
+
+actualizarCarrito();
 
 }
