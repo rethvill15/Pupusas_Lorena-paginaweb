b45830e8f390679e4d81f4f97e7c5005fe580d3f
Aldo Villagra <afdanic@gmail.com>
2026-09-03 13:52:23 -0600

    UUUUH, aparecio un conflicto de merger.

--- JS_archivos/menu.js
+++ JS_archivos/menu.js
@@ -57,37 +57,16 @@ const imagenesProductos = {
         COMBOS
     -----------------------------------------------------------*/
 
-<<<<<<< HEAD
-    1: "Menu_pupusas/Imagenes_menu/20_pupusas.JPG",
-    2: "Menu_pupusas/Imagenes_menu/12_pupusas.png",
-    3: "Menu_pupusas/Imagenes_menu/5_pupusas.jpg",
-    4: "Menu_pupusas/Imagenes_menu/3_pupusas.jpg",
-=======
     1: "Imagenes_menu/20_pupusas.JPG",
     2: "Imagenes_menu/12_pupusas.png",
     3: "Imagenes_menu/5_pupusas.jpg",
     4: "Imagenes_menu/3_pupusas.jpg",
->>>>>>> c0a8c3d9be5782c61fe21efb69ae46b6d8d540eb
 
 
     /*-----------------------------------------------------------
         PUPUSAS
     -----------------------------------------------------------*/
 
-<<<<<<< HEAD
-    5:  "Menu_pupusas/Imagenes_menu/Pupusa_queso_sencilla.png",
-    6:  "Menu_pupusas/Imagenes_menu/pupusa-de-pollo.png",
-    7: "Menu_pupusas/Imagenes_menu/Pupusa_queso_3quesos.jpg",
-    8: "Menu_pupusas/Imagenes_menu/Pupusa_carnes_mixtas.jpg",
-    9: "Menu_pupusas/Imagenes_menu/pupusa_pollo_queso.jpg",
-    10: "Menu_pupusas/Imagenes_menu/Pupusa_queso_frijol.jpg",
-    11: "Menu_pupusas/Imagenes_menu/Pupusa_chicharron_frijol.jpg",
-    12: "Menu_pupusas/Imagenes_menu/Pupusa_picante.jpg",
-    13: "Menu_pupusas/Imagenes_menu/Pupusa_queso_chicharron.jpg",
-    14: "Menu_pupusas/Imagenes_menu/Pupusa_pollo_frijol.jpg",
-    15: "Menu_pupusas/Imagenes_menu/Pupusa_todo_uno.jpg",
-    16: "Menu_pupusas/Imagenes_menu/Pupusa_gigante.jpg",
-=======
     5:  "Imagenes_menu/Pupusa_queso_sencilla.png",
     6:  "Imagenes_menu/pupusa-de-pollo.png",
     7: "Imagenes_menu/Pupusa_queso_3quesos.jpg",
@@ -100,41 +79,33 @@ const imagenesProductos = {
     14: "Imagenes_menu/Pupusa_pollo_frijol.jpg",
     15: "Imagenes_menu/Pupusa_todo_uno.jpg",
     16: "Imagenes_menu/Pupusa_gigante.jpg",
->>>>>>> c0a8c3d9be5782c61fe21efb69ae46b6d8d540eb
 
 
     /*-----------------------------------------------------------
         POSTRES
     -----------------------------------------------------------*/
 
-<<<<<<< HEAD
-    17: "Menu_pupusas/Imagenes_menu/Cheesecake.jpg",
-    18: "Menu_pupusas/Imagenes_menu/Picos_nica.png",
-=======
     17: "Imagenes_menu/Cheesecake.jpg",
     18: "Imagenes_menu/Picos_nica.png",
->>>>>>> c0a8c3d9be5782c61fe21efb69ae46b6d8d540eb
 
 
     /*-----------------------------------------------------------
         BEBIDAS
     -----------------------------------------------------------*/
 
-<<<<<<< HEAD
-    19: "Menu_pupusas/Imagenes_menu/Coca Cola.jpg",
-    20: "Menu_pupusas/Imagenes_menu/Sprite.jpg",
-    21: "Menu_pupusas/Imagenes_menu/Te Frio_lipton.jpg",
-    22: "Menu_pupusas/Imagenes_menu/Jugo_naranja.jpg",
-    23: "Menu_pupusas/Imagenes_menu/Fresco_Jamaica.jpg",
-    24: "Menu_pupusas/Imagenes_menu/Limonada.jpg"
-=======
+    19: "Imagenes_menu/Coca Cola.jpg",
+    20: "Imagenes_menu/Sprite.jpg",
+    21: "Imagenes_menu/Te Frio_lipton.jpg",
+    22: "Imagenes_menu/Jugo_naranja.jpg",
+    23: "Imagenes_menu/Fresco_Jamaica.jpg",
+    24: "Imagenes_menu/Limonada.jpg",
     19: "Imagenes_menu/Coca Cola.jpg",
     20: "Imagenes_menu/Sprite.jpg",
     21: "Imagenes_menu/Te Frio_lipton.jpg",
     22: "Imagenes_menu/Jugo_naranja.jpg",
     23: "Imagenes_menu/Fresco_Jamaica.jpg",
     24: "Imagenes_menu/Limonada.jpg"
->>>>>>> c0a8c3d9be5782c61fe21efb69ae46b6d8d540eb
+
 };
 
 
